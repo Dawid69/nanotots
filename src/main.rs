27b844d8f92add555fc398539c024a68c19ef0d7
Dawid <jordaandvd at gmail>
2022-04-20 17:06:23 +0200

    Moved over to clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
-use std::env;
-
 use chrono::prelude::Local;
 use chrono::TimeZone;
+use clap::crate_authors;
+use clap::crate_description;
+use clap::crate_version;
+use clap::{Arg, Command};
 
 fn main() {
+    let arguments = Command::new("Test nanotots")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::new("timestamp_nanos")
+                .short('n')
+                .long("nano")
+                .value_name("TIMESTAMP_NANOS")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let args: Vec<String> = env::args().collect();
+    match arguments.occurrences_of("timestamp_nanos") {
+        0 => println!("No args supplied"),
+        1 => {
+            let stamp = arguments.value_of("timestamp_nanos").unwrap();
+            let dt = chrono::Local.timestamp_nanos(stamp.parse().unwrap());
 
-
-    let dt = chrono::Local.timestamp_nanos(args[1].parse::<i64>().unwrap());
-
-    println!("Local time: {}\n", Local::now());
-    println!("Input time: {}\n", dt);
+            println!("Local time: {}\n", Local::now());
+            println!("Input time: {}\n", dt);
+        }
+        _ => todo!(),
+    }
 }
